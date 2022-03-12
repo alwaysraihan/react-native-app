4d998f3526c346c723dd33348596d30fdf83db4e
Raihan Sarkar <bdyoungdeveloper@gmail.com>
2022-03-12 13:18:42 +0600

    simple about contact userdata course component crreate and imported in app.js

--- App.js
+++ App.js
@@ -1,19 +1,30 @@
 import React from 'react';
-
-import Home from './src/screens/Home';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+// all useful screen import 
+import Home from './src/screens/Home';
+import About from './src/screens/About';
+import Contact from './src/screens/Contact';
+import Course from './src/screens/Course';
+import UserData from './src/screens/UserData';
 
 export default function App() {
   const Stack = createNativeStackNavigator();
   return <NavigationContainer>
     <Stack.Navigator initialRouteName='Home'>
-      {/* <Stack.Screen name='Home' component={Home}/> */}
+      {/* Home screen  */}
       <Stack.Screen name='Home'> 
-      {(props)=> <Home {...props} tutorialName={'Raihan'}/>}
-
+      {(props)=> <Home {...props} tutorialName={'Raihan Tutorilal'}/>}
       </Stack.Screen>
+
+      {/* Course screen  */}
+      <Stack.Screen name='Course' component={Course}/>
+
+      {/* About screen  */}
+      <Stack.Screen name='About' component={About}/>
+
+      {/* Contact screen  */}
+      <Stack.Screen name='Contact' component={Contact}/>
     </Stack.Navigator>
   </NavigationContainer>
 }
