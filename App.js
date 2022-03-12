c8a2c03d16914e3f37e3d0e2e3aa510a58b55ca1
Raihan Sarkar <bdyoungdeveloper@gmail.com>
2022-03-12 13:02:44 +0600

    home screen image added

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+import Home from './src/screens/Home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const Stack = createNativeStackNavigator();
+  return <NavigationContainer>
+    <Stack.Navigator initialRouteName='Home'>
+      {/* <Stack.Screen name='Home' component={Home}/> */}
+      <Stack.Screen name='Home'> 
+      {(props)=> <Home {...props} tutorialName={'Raihan Tutorial'}/>}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      </Stack.Screen>
+    </Stack.Navigator>
+  </NavigationContainer>
+}
---
+++ src/screens/Home.js
@@ -0,0 +1,79 @@
+import { StyleSheet, Text, View, Image} from 'react-native';
+import React from 'react'
+
+const Home = (props) => {
+  const description= 'Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
+
+  return (
+    <View style={styles.mainContainer}>
+      <View style={styles.homeTop}>
+    
+        <Image
+         style={styles.headerImage}
+         resizeMode="contain"
+        source={require("../../assets/computer.jpg")}
+        />
+        <Text style={styles.headerTitle}>Welcome To</Text>
+        <Text 
+        style={[
+          styles.headerTitle,
+          {
+            fontSize:33,
+            color: "#4c5dab",
+            marginTop: 0,
+            },
+            ]}>
+             {props.tutorialName}
+          </Text>
+          <Text style={styles.paraStyle}>
+            {description}
+            
+          </Text>
+      </View>
+    </View>
+      );
+    }
+    
+    const styles = StyleSheet.create({
+      maincontainer: {
+       height:'100%',
+       display:'flex',
+       justifyContent:'space-between',
+       paddingHorizontal:20,
+       backgroundColor:'#fff',
+       textAlign:'center',
+       
+      },
+      homeTop:{
+      display:'flex',
+      justifyContent:'center',
+      alignItems:'center',
+      paddingHorizontal:10,
+      },
+      headerTitle:{
+        fontSize:30,
+        color:'#344055',
+        textTransform:'uppercase',
+      },
+      paraStyle:{
+        textAlign:'left',
+        fontSize:19,
+        color:'#7d7d7d',
+        marginTop:30,
+        paddingBottom:50,
+        lineHeight:26,
+      },
+    
+      headerImage: {
+    height:undefined,
+    width:'100%',
+    aspectRatio:1,
+    display:'flex',
+    alignItems:'stretch',
+    marginTop:50,
+    borderRadius:20
+      },
+    
+    });
+    
+export default Home
